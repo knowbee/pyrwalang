6835a09202cc929689c97f86b0633985abc83818
knowbee <knowbeeinc@gmail.com>
2020-12-25 18:02:43 +0200

    version 0.0.5

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name="rwalang",
-    version='0.0.2',
+    version='0.0.5',
+    url="https://github.com/knowbee/pyrwalang.git",
     description="A fast language detection package for Ikinyarwanda(Native language of Rwandans)",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     py_modules=["rwalang"],
-    packages = find_packages(),
+    packages=find_packages(),
     keywords=["detect language",
               "rwanda",
               "ikinyarwanda",
